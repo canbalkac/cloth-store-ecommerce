3d03d8b0ff43ae46aa07801d8624f0a9a11f7553
CAN BALKAÇ <78991668+canbalkac@users.noreply.github.com>
2025-05-30 22:44:24 +0300

    unused import deleted

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { cart } from "@/constants/constants";
