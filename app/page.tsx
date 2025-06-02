9a44199db2be4f0d5315e5ab61c840da64183fef
CAN BALKAÇ <78991668+canbalkac@users.noreply.github.com>
2025-06-02 22:33:59 +0300

    home page ui update and build

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import CategoryMenu from "@/components/home/CategoryMenu";
 import NewCollection from "@/components/home/NewCollection";
 import NewThisWeek from "@/components/home/NewThisWeek";
 import XIVCollections from "@/components/home/XIVCollections";
@@ -7,7 +6,6 @@ import About from "@/components/home/About";
 export default function Home() {
   return (
     <>
-      <CategoryMenu />
       <NewCollection />
       <NewThisWeek />
       <XIVCollections />
